ab1b0df0b2fb0a1aef41fbd13e99fcae7e725b26
krisi3 <50706023+krisi3@users.noreply.github.com>
2023-08-23 15:47:20 +0300

    Update exercise1-fix.sh

--- materials/vagrant/fixScripts/exercise1-fix.sh
+++ materials/vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-#add fix to exercise1 here
+# Perform network tests
+curl http://www.textfiles.com/art/bnbascii.txt
+ping 8.8.8.8
+ping www.textfiles.com
+ip route
+route -n
+
+# Install 'net-tools' package
+sudo apt install -y net-tools
+
+# Display routes again
+route -n
+
+# Delete specific route
+sudo ip route del 208.86.224.90 dev enp0s8
+
+# Perform curl after deleting the route
+curl http://www.textfiles.com/art/bnbascii.txt
